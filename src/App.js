74cdd889dba1e87b7a0de46cafd417359973f230
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-05-28 20:00:18 -0500

    added face models

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import * as faceApi from 'face-api.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const faceModels = {
+  anger: "🤬",
+  surprise: "😲",
+  fear: "😖",
+  neutral: "😶",
+  happy: "😄",
+  sad: "😞"
+};
