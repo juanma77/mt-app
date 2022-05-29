5699ede660de0554495904a3243edc5ee8745dae
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-05-28 20:05:09 -0500

    running async task to load models from api

--- src/App.js
+++ src/App.js
@@ -12,6 +12,36 @@ const faceModels = {
 
 class App extends React.Component {
 
+  startVideo = React.createRef();
+
+  state = { 
+    models: [] 
+  };
+
+  componentDidMount() {
+
+    this.run();
+
+  }
+
+  run = async () => {
+   
+    try {
+
+      await faceApi.nets.tinyFaceDetector.load("/models/");
+
+      await faceApi.loadFaceExpressionModel(`/models/`);
+      
+      this.mediaStream = await navigator.mediaDevices.getUserMedia({
+        startVideo: { facingMode: "user" }
+      });
+
+      this.startVideo.current.srcObject = this.mediaStream;
+    } catch (e) {
+      this.log(e.name, e.message, e.stack);
+    }
+  };
+
 
 
 }
