e4a82024559401e5c1281f5aae74e2fb11417c45
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-02 00:42:59 -0500

    updating app.js file to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import * as faceApi from 'face-api.js';
 
 const faceModel = {
@@ -13,7 +13,7 @@ const faceModel = {
 
 };
 
-class App extends React.Component {
+class App extends Component {
 
   video = React.createRef();
 
