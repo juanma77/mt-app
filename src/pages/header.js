fd725069df285030ab9a7867fc4d7ab0101890cd
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-05 17:11:14 -0500

    addded menu to header

---
+++ src/pages/header.js
@@ -0,0 +1,32 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Header = () => {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Face Recognition App</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        
+                        <li className="nav-item active">
+                            <Link to="/"> Home &nbsp; 
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <Link to="/dashboard">Dashboard</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        <Outlet/>
+        </>
+    )
+};
+
+export default Header;
