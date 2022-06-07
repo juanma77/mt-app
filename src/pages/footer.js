f43f66f63883684a873988d0689cbbdb06f952e1
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-07 11:50:06 -0500

    footer added

---
+++ src/pages/footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+
+    return (
+        <>  
+            <br/><br/><br/><br/><br/>
+            <div className="container-footer" style={ { textAlign: 'center' } }>
+                <div className="card-body">
+                    <h5 className="card-title">Todos los derechos reservados</h5>
+                    <p className="card-text">Hecho por JuanMa</p>    
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Footer;
