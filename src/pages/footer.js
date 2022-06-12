c26e75cd9350bc076f13a8477ee6b9234ccdfcab
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-11 20:35:59 -0500

    updating text

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,12 +1,11 @@
 const Footer = () => {
-
     return (
         <>  
             <br/><br/><br/><br/><br/>
             <div className="container-footer" style={ { textAlign: 'center' } }>
                 <div className="card-body">
-                    <h5 className="card-title">All right reserved</h5>
-                    <p className="card-text">Made by Juan Manuel López Santana for his Universidad de Burgos Master's Degree Thesis</p>    
+                    <h5 className="card-title">Todos los derechos reservados</h5>
+                    <p className="card-text">Hecho por Juan Manuel López Santana para su Trabajo Final de Máster por la Universidad de Burgos</p>    
                 </div>
             </div>
         </>
