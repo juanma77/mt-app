a6299f24f47142c90125e002470c2ee908cfa867
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-11 20:36:54 -0500

    deleting white spaces and updating text

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,28 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
 
 const Header = () => {
-
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">Intrusion Detection Application</a>
+                <a className="navbar-brand" href="/">Aplicación de Detección de Intrusos</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        
                         <li className="nav-item active">
-                            <Link to="/"> Home &nbsp; </Link>
+                            <Link to="/"> Inicio &nbsp; </Link>
                         </li>
-                        
                         <li className="nav-item">
-                            <Link to="/about">About &nbsp; </Link>
+                            <Link to="/about">Acerca De &nbsp; </Link>
                         </li>
-
                         <li className="nav-item">
-                            <Link to="/contact">Contact </Link>
+                            <Link to="/contact">Contacto </Link>
                         </li>
                     </ul>
                 </div>
