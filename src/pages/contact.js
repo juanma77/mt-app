e0698d553ad3ab8968189a756540404b8d3d318a
Juan Manuel López Santana <freakshield777@hotmail.com>
2022-06-11 20:34:53 -0500

    deleting white spaces

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,64 +1,38 @@
 const Contact = () => {
-
     return (
         <>  
             <br/><br/><br/><br/><br/>
-
             <div class="container">
- 
                 <div class="row">
                     <div class="col">
                         <div className="card" style={{ width: '18rem' }}>
-
                             <a href="https://github.com/juanma77"><img src={ "../images/github.png" } className="card-img-top" alt="Author GitHub page " /></a>
-
                             <div className="card-body">
                                 <h5 className="card-title">GitHub</h5>
                             </div>
-
                         </div>
-
                     </div>
 
                     <div class="col">
-
                         <div className="card" style={{ width: '18rem' }}>
-
                             <img src={ "../images/author.PNG" } className="card-img-top" alt="Author GitHub page " />
-
                             <div className="card-body">
                                 
-                                <h5 className="card-title">Author</h5>
+                                <h5 className="card-title">Autor</h5>
                             </div>
-
                         </div>
-                    
                     </div>
 
                     <div class="col">
-
                         <div className="card" style={{ width: '18rem' }}>
-
                             <a href="https://www.linkedin.com/in/juan-manuel-l%C3%B3pez-santana-094486104/"><img src={ "../images/linkedin.png" } className="card-img-top" alt="Author Linkedin page " /></a>
-
                             <div className="card-body">
                                 <h5 className="card-title">LinkedIn</h5>
                             </div>
-
                         </div>
-                    
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
-
-
-            
         </>
     )
 };
